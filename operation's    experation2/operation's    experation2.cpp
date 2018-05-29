24cc211bf3b50d258b7fae98da5b8b479aa814b2
20172104254 <15750545478@163.com>
2018-05-29 14:57:58 +0800

    Initial commit

---
+++ operation's    experation2/operation's    experation2.cpp
@@ -0,0 +1,50 @@
+// operation's    experation2.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"iostream"
+using namespace std;
+class CFeet
+{
+private:
+	int feet;
+	int inches;
+public:
+	void setvalue(int f, int i);
+	void display();
+	CFeet operator-(CFeet &a);
+};
+void CFeet::setvalue(int f, int i)
+{
+	feet = f + i / 12;
+	inches = i % 12;
+}
+void CFeet::display()
+{
+	cout << feet << "英尺" << inches << "英寸" << endl;
+}
+CFeet CFeet::operator-(CFeet&a)
+{
+	CFeet temp;
+	if (feet > 0)
+	{
+		temp.setvalue(feet - a.feet, inches - a.inches);
+	}
+	if (feet < 0 || feet == 0)
+	{
+		temp.setvalue(feet - a.feet, feet * 12 + inches - a.feet * 12 - a.inches);
+	}
+
+
+	return temp;
+}
+
+int main()
+{
+	CFeet A, B, C;
+	A.setvalue(2, 2);
+	B.setvalue(1, 1);
+	C = B - A;
+	C.display();
+	return 0;
+}
